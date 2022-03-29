aa389a420bc1b4acf5648668b83ad19d9b0f10b9
Rylen Lanning <rlanning@greylionsoftware.com>
2022-03-29 12:19:46 -0500

    Initial Commit

---
+++ WordleBotApp/WordleBotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleBotAppApp.swift
+//  WordleBotApp
+//
+//  Created by Rylen Lanning on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleBotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
